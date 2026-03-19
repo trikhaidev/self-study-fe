2cd26b6a8ce232dae8d7c7e5b26ecaf2f6803727
trikhaidev <trikhai.dev@gmail.com>
2026-03-19 22:41:06 +0700

    tìm hiểu tổng quan react

---
+++ React/Day001/main.js
@@ -0,0 +1,7 @@
+// import {myName} from "./service.js";
+
+import * as service from "./service.js";
+
+console.log(service.myName);
+
+service.sayHello(3);
---
+++ React/Day001/service.js
@@ -0,0 +1,9 @@
+export const myName = 'Trí Khải';
+
+export const sayHello = (loopCount) => {
+    for(let i = 0; i < loopCount; i++) {
+        console.log('Hello, I am ' + myName);
+    }
+};
+
+//export {myName, sayHello};
