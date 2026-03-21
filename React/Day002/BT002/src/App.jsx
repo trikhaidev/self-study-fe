00295693cd7246c8ab9deae7439861175d182b8c
trikhaidev <trikhai.dev@gmail.com>
2026-03-21 21:57:01 +0700

    Tìm hiểu: Component trong React
    Link video: https://youtu.be/nFDpu-ill-8?list=PLPt6-BtUI22oD3xfWy9VI9klNNxqAnTjb

---
+++ React/Day002/BT002/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function Header() {
+  return (
+    <>
+      <h1>Số 5</h1>
+    </>
+  );
+}
+
+function Sum() {
+  return <>
+    <h1>Tổng</h1>
+  </>;
+}
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <h1>Hello World!</h1>
+      <Sum />
+    </>
+  )
+}
+
+export default App
