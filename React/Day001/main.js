e040ffa76f92a463ac9f07985e7c10a4c166a5db
trikhaidev <trikhai.dev@gmail.com>
2026-03-21 20:59:53 +0700

    tìm hiểu khởi tạo project react
    Lưu ý: Hoàn toàn có thể khởi tạo project react với ngôn ngữ lập trình TS. Khá giống Angular đã học trước đó

--- React/Day001/main.js
+++ React/Day001/main.js
@@ -19,15 +19,21 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // import * as method from "./service.js";
-import func from "./service.js";
-import defaultExport,{} from "./service.js";   
+// import func from "./service.js";
+// import defaultExport,{} from "./service.js";   
 // import {sum} from "./service.js";
 
 // const number = method.sum(5,10);
 
-var number = func(5,10);
+// var number = func(5,10);
 // var numberMethod = method.default(100, 36);
 // var sum3 = sum(3, 4);
 // console.log("Sum: "+number);
-console.log(`number method: ${numberMethod}`);
+//console.log(`number method: ${numberMethod}`);
 // console.log(`Sum 3: ${sum3}`);
+
+import func, * as service from "./service.js";
+import f from "./service.js";
+service.sayHelloWorld(3);
+console.log("Sum: "+func(7,7));
+console.warn("Sum f: " + func(5,5));
