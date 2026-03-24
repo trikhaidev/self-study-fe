1531b7e70e5a42f29f151fa483800124e83436df
trikhaidev <trikhai.dev@gmail.com>
2026-03-24 22:55:11 +0700

    React js _ Props Trong React
    Link video: https://youtu.be/XhfRaoa_YmM?list=PLPt6-BtUI22oD3xfWy9VI9klNNxqAnTjb

--- React/Day004/src/components/content/content-component.tsx
+++ React/Day004/src/components/content/content-component.tsx
@@ -1,13 +1,12 @@
 import './content-component.css'
+import img1 from "../../assets/img1.png"
+import img2 from "../../assets/img2.jpg"
+import img3 from "../../assets/img3.jpeg"
+import img4 from "../../assets/img4.jpg"
 
-const images = [
-    "https://dthezntil550i.cloudfront.net/n4/latest/n41611050401464760001833695/1280_960/1128ba0a-efcb-4370-97cf-7704d4c0555d.png",
-    "https://www.vietnamworks.com/hrinsider/wp-content/uploads/2023/12/anh-den-ngau-003.jpg",
-    "https://www.vietnamworks.com/hrinsider/wp-content/uploads/2023/12/anh-den-ngau.jpeg",
-    "https://www.vietnamworks.com/hrinsider/wp-content/uploads/2023/12/anh-den-ngau-002.jpg",
-]
+const images = [img1,img2,img3,img4]
 
-function randomImage(){
+function randomImage():string{
     return images[Math.floor(Math.random() * images.length)];
 }
 
@@ -17,12 +16,29 @@ function Img(){
     </>
 }
 
+function Text(model:TextModel){
+    return (
+        <>
+            <p style={{color:model.color ?? 'black'}}>{model.text}</p>
+        </>
+    );
+}
+
 export default function Content(){
     return (
         <>
             <div id='content'>
                 <Img></Img>
+                <hr />
+                <Text text= "Hello bro! ok bro" color="blue"></Text>
+                <hr />
+                <Text text={'Are You Ok?'} color={'Yellow'}/>
             </div>
         </>
     );
+}
+
+class TextModel{
+    public text:string|undefined|null;
+    public color:string|undefined|null;
 }
