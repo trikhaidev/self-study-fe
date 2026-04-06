d8976c0552893ed23a1f3cde1083e4b65b2c7d17
trikhaidev <trikhai.dev@gmail.com>
2026-04-06 19:31:45 +0700

    Keeping Components Pure
    Link: https://react.dev/learn/keeping-components-pure

--- React/ComponentsPure/src/App.tsx
+++ React/ComponentsPure/src/App.tsx
@@ -3,13 +3,31 @@ import SayHello from "./components/SayHello"
 function App() {
   return (
     <>
-      <SayHello></SayHello>
+      <SayHello loop={3}></SayHello>
       <hr />
-      <SayHello></SayHello>
-      hr
-      <SayHello></SayHello>
+      <SayHello loop={3}></SayHello>
     </>
   )
 }
 
 export default App
+
+    // nextCount(); - Gọi hàm này => Count sẽ được gán lại giá trị mới => không tuân thủ nguyên tắc Keep Components Pure.
+    // count = count + 1; - Cách này cũng tương tự như trên, tuy nhiên nó sẽ bị ESlint phát hiện và cảnh báo.
+    // const random = Math.random(); // cũng không Pure => do nó trả ra các kết quả khác nhau mỗi lần gọi
+    // new Date().toLocaleTimeString(): cũng không Pure vì nó trả ra kết quả khác mỗi lần gọi ở những thời điểm khác nhau.
+
+    // => Dù tất cả các cách đều không tuân thủ quy tắc Keep Components Pure của React nhưng trên thực tế chương trình vẫn chạy bình thường và không hề có lỗi nào xảy ra.
+    // => Ta nên tuân thủ Keep Components Pure để tránh những lỗi khó hiểu, khó phát hiện, khó kiểm soát và không mong muốn.
+
+    // Một function component được coi là Pure khi nó luôn trả về cùng một kết quả cho một cùng một input và nó sẽ không
+    //tác đến đến bất kì biến, đối tượng, mảng, ... nào đã được khai báo trước đó. Xem rõ hơn ở https://react.dev/learn/keeping-components-pure
+
+    /**
+     * Lưu ý quan trọng:
+        - React không đảm bảo thứ tự render giữa các component.
+        Mỗi component phải:
+            + “Tự suy nghĩ”
+            + Không phụ thuộc component khác trong lúc render
+        - Render giống như bài thi: mỗi component tự làm bài của mình.
+     */
--- React/ComponentsPure/src/components/SayHello.tsx
+++ React/ComponentsPure/src/components/SayHello.tsx
@@ -1,24 +1,59 @@
-let count = 0;
-export default function SayHello(){
-    nextCount();
+import type { ReactNode } from "react";
+
+let count = 0; 
+count++;
+
+type SayHelloProps = {
+    loop:number
+}
+export default function SayHello(p:SayHelloProps){
+    // nextCount(); - Gọi hàm này => Count sẽ được gán lại giá trị mới => không tuân thủ nguyên tắc Keep Components Pure.
+    // count = count + 1; - Cách này cũng tương tự như trên, tuy nhiên nó sẽ bị ESlint phát hiện và cảnh báo.
+    // const random = Math.random(); // cũng không Pure => do nó trả ra các kết quả khác nhau mỗi lần gọi
+    // new Date().toLocaleTimeString(): cũng không Pure vì nó trả ra kết quả khác mỗi lần gọi ở những thời điểm khác nhau.
+
+    // => Dù tất cả các cách đều không tuân thủ quy tắc Keep Components Pure của React nhưng trên thực tế chương trình vẫn chạy bình thường và không hề có lỗi nào xảy ra.
+    // => Ta nên tuân thủ Keep Components Pure để tránh những lỗi khó hiểu, khó phát hiện, khó kiểm soát và không mong muốn.
+
+    // Một function component được coi là Pure khi nó luôn trả về cùng một kết quả cho một cùng một input và nó sẽ không
+    //tác đến đến bất kì biến, đối tượng, mảng, ... nào đã được khai báo trước đó. Xem rõ hơn ở https://react.dev/learn/keeping-components-pure
+
+    /**
+     * Lưu ý quan trọng:
+        - React không đảm bảo thứ tự render giữa các component.
+        Mỗi component phải:
+            + “Tự suy nghĩ”
+            + Không phụ thuộc component khác trong lúc render
+        - Render giống như bài thi: mỗi component tự làm bài của mình.
+     */
+    console.log("SayHello render");
+    const array:ReactNode[] = [];
+    for(let i = 0; i < p.loop; i++){
+        array.push(<SomeThing n={i + 1} key={i + 1}></SomeThing>);
+    }
     return (
         <>
             <h1>Hello world {count} - {new Date().toLocaleTimeString()}</h1>
             <hr />
-            <SomeThing n = {1}></SomeThing>
-            <SomeThing n = {2}></SomeThing>
-            <SomeThing n = {3}></SomeThing>
+            {/* <SomeThing n = {count + 1}></SomeThing>
+            <SomeThing n = {count + 2}></SomeThing>
+            <SomeThing n = {count + 3}></SomeThing> */}
+            {/* <SomeThing n = {random}></SomeThing> */}
+
+            {array}
         </>
     );
 }
-function nextCount(){
-    count = count + 1;
-}
+
 type SomeThingProps = {
     n:number
 }
 function SomeThing(p :SomeThingProps){
     return <p>
-        {p.n}
+        Some thing {p.n}
     </p>
 }
+
+// function nextCount(){
+//     count = count + 1;
+// }
