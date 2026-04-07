d05ff1bfc936f111ee15a37cf835a8ce48184980
trikhaidev <trikhai.dev@gmail.com>
2026-04-07 22:11:43 +0700

    Responding to Events

---
+++ React/RespondingToEvents/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello Tailwind!
+      </h1>
+    </>
+  )
+}
+
+export default App
