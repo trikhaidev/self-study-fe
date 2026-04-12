4329d042a79b8dad0a9aa8b52521b730f0c564d3
trikhaidev <trikhai.dev@gmail.com>
2026-04-12 22:45:19 +0700

    State: A Component's Memory
    Link: https://react.dev/learn/state-a-components-memory

--- React/ComponentMemory/src/App.tsx
+++ React/ComponentMemory/src/App.tsx
@@ -2,43 +2,47 @@ import React, { useState } from "react"
 
 function App() {
   const [text, setText] = useState('');
-  function handleInput(e: React.InputEvent<HTMLInputElement>){
+  function handleInput(e: React.InputEvent<HTMLInputElement>) {
     setText(e.currentTarget.value);
   }
 
-  const [y,setY] = useState(10);
+  const [y, setY] = useState(10);
   const [x, setX] = useState(0);
-  function handleMouseMove(e:React.MouseEvent<HTMLInputElement>) {
+  function handleMouseMove(e: React.MouseEvent<HTMLInputElement>) {
     console.log(`${e.clientX} - ${e.clientY}`);
     setX(e.clientX);
     setY(e.clientY);
   }
-
-return (
-  <>
-    <div style={
-      {
-        fontSize:50
+  const [width, setWith] = useState(250);
+  const [height, setHeight] = useState(250);
+  return (
+    <>
+      <div style={
+        {
+          fontSize: 50
+        }
+      }>
+        <input type="text" onInput={handleInput} value={text} style={{ height: 50, width: 300, fontSize: 50 }} />
+        <p>Content: {text}</p>
+        <input type="text" onInput={handleInput} value={text} style={{ height: 50, width: 300, fontSize: 50 }} />
+      </div><hr />
+      <button onClick={() => setHeight(height - 10)}>-</button>
+      <button onClick={() => setHeight(height + 10)}>+</button>
+      <div style={
+        {
+          width: width,
+          height: height,
+          border: '1px solid black',
+          background: `rgb(100,${x},${y})`
+        }
       }
-    }>
-      <input type="text" onInput={handleInput} value={text} style={{height:50,width:300,fontSize:50}}/> 
-      <p>Content: {text}</p>
-      <input type="text" onInput={ handleInput} value={text} style={{height:50,width:300,fontSize:50}}/>
-    </div><hr />
-    <div style={
-      {
-        width:250,
-        height:250,
-        border: '1px solid black',
-        background: `rgb(100,${x},${y})`
-      }
-    }
-    onMouseMove={handleMouseMove}
-    >
-
-    </div>
-  </>
-)
+        onMouseMove={handleMouseMove}
+      >
+      </div>
+      <button onClick={() => setWith(width - 10)}>-</button>
+      <button onClick={() => setWith(width + 10)}>+</button>
+    </>
+  )
 }
 
 export default App
