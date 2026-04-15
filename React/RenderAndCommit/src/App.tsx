116fae1c60e1fe53a783251dda4e90fcfc9a686b
trikhaidev <trikhai.dev@gmail.com>
2026-04-15 20:55:37 +0700

    Render End Commit: - Kích hoạt Render: là khi component render lần đầu tiên hoặc khi state của nó bị thay đổi hoặc khi component cha của nó render lại. - Render: React sẽ gọi các component cần thay đổi. Khi render xong React sẽ chưa cập nhật vào DOM ngay. -Commit: React sẽ so sánh các thay đổi. DOM chỉ được cập nhật khi có sự thay đổi và React chỉ cập nhật những điều cần thiết Lưu ý: - Ở bước cuối (Commit), vì là React chỉ cập nhật những thứ cần thiết nên là giá của input, textarea sẽ không bị mất. - Quá trình rerender diễn ra => các function Component thực sự sẽ được chạy lại. Khi quá trình render hoàn tất, React sẽ so sánh kết quả render với DOM hiện tại và chỉ cập nhật những thứ cần thiết.
    Link: https://react.dev/learn/render-and-commit

--- React/RenderAndCommit/src/App.tsx
+++ React/RenderAndCommit/src/App.tsx
@@ -3,15 +3,14 @@ import Input from "./components/Input";
 
 function App() {
   const [number, setNumber] = useState(0);
-  if (number === 0) {
-    setInterval(() => {
-      setNumber((prev) => prev + 1);
-    }, 1000);
-  }
-  console.log("App tsx");
+  console.log("App tsx: "+number);
   return (
     <>
+    <button onClick={() => setNumber((prev) => prev + 1)}>Increment</button>
+      <hr />
       <input type="text" />
+      <hr />
+      <Input number={number}></Input>
       <hr />
       <Input number={number}></Input>
     </>
--- React/RenderAndCommit/src/components/Input.tsx
+++ React/RenderAndCommit/src/components/Input.tsx
@@ -2,8 +2,12 @@ type InputProps = {
     number:number;
 }
 export default function Input(p:InputProps){
-    const width = `${p.number * 2}px`;
+    const width = `${p.number}px`;
     console.log("Input width: " + width);
+    // if(p.number % 2 !== 0){
+    //     console.log("Odd");
+    //     return <h1>Odd number: {p.number}</h1>;
+    // }
     return (
         <>  
             <h1>{width}</h1>
