1810f2bb45b2c4d4f876a0f4d8c58cf6ee45e542
trikhaidev <trikhai.dev@gmail.com>
2026-04-25 22:43:14 +0700

    Bài tập: làm một trang sản phẩm đơn giản. Yêu cầu: hiển thị danh sách, tìm kiếm sản phẩm, xem chi tiết sản phẩm. Phải sử dụng state để quản lý các trạng thái.
    Mục đích bài tập: Luyện tập quản lý state

--- React/VerySimpleStore/src/components/ShowProduct.tsx
+++ React/VerySimpleStore/src/components/ShowProduct.tsx
@@ -26,11 +26,21 @@ export default function ShowProduct({ product, onClose }: ShowProductProps) {
                     >Close</button>
                 </div>
                 <h1>{product.title}</h1>
+                <h3>${product.price.toFixed(2)}</h3>
                 <p>{product.description}</p>
-                <p>{product.price}</p>
                 {
                     product.images.map((image, index) => (
                         <img key={index} src={image} alt={product.title} className="showProductDetail" />
+                    ))
+                }
+                <hr />
+                {
+                    product.reviews.map((review, index) => (
+                        <div key={index} className="review">
+                            <h4>{review.reviewerName} - {review.rating} stars</h4>
+                            <p>{review.comment}</p>
+                            <small>{review.date}</small>
+                        </div>
                     ))
                 }
             </div>
