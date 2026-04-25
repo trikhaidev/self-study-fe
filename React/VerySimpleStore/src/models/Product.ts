d75bddc6b5adf023bb27cb35feaaebeb85bd6141
trikhaidev <trikhai.dev@gmail.com>
2026-04-25 10:29:45 +0700

    .

---
+++ React/VerySimpleStore/src/models/Product.ts
@@ -0,0 +1,45 @@
+export default interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: Dimensions;
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: Review[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: Meta;
+  images: string[];
+  thumbnail: string;
+}
+
+export interface Dimensions {
+  width: number;
+  height: number;
+  depth: number;
+}
+
+export interface Review {
+  rating: number;
+  comment: string;
+  date: string; // hoặc Date
+  reviewerName: string;
+  reviewerEmail: string;
+}
+
+export interface Meta {
+  createdAt: string; // hoặc Date
+  updatedAt: string; // hoặc Date
+  barcode: string;
+  qrCode: string;
+}
