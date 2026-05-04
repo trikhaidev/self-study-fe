b603452f8c2dee95ba8a3f963d7cde93d7a84fe6
trikhaidev <trikhai.dev@gmail.com>
2026-05-04 21:17:32 +0700

    Preserving and Resetting State
    Link: https://react.dev/learn/preserving-and-resetting-state

--- React/KeepState/src/App.tsx
+++ React/KeepState/src/App.tsx
@@ -5,20 +5,23 @@ import HelloWorld from "./components/HelloWorld";
 
 function App() {
   const [check, setcheck] = useState(true);
-  if (check) {
-    return (
-      <>
-        <HelloWorld show={!check}></HelloWorld>
-        {check ? <p>check true</p> : ' asas  as as asasd'}
-        <hr />
-        <Count check={check}></Count>
-        <Count check={check}></Count>
-        <input type="checkbox" checked={check} onChange={e => setcheck(e.currentTarget.checked)} />
-        <hr />
-        <input type="text" key='ok'/>
-      </>
-    );
-  }
+  // if (check) {
+  //   return (
+  //     <>
+  //       <HelloWorld show={!check}></HelloWorld>
+  //       {check ? <p>check true</p> : ' asas  as as asasd'}
+  //       <hr />
+  //       <Count check={check}></Count>
+  //       <Count check={check}></Count>
+  //       <input type="checkbox" checked={check} onChange={e => setcheck(e.currentTarget.checked)} />
+  //       <hr />
+  //       <input type="text"/>
+  //     </>
+  //   );
+  // }
+  /**
+   * Khi thẻ cha biến mất thì state của nó sẽ bị xóa,  các thẻ con bên trong nó cũng sẽ bị xóa state luôn
+   */
   return (
     <>
       <HelloWorld show={!check}></HelloWorld>
@@ -26,7 +29,18 @@ function App() {
       <Count check={check}></Count>
       <Count check={check}></Count>
       <input type="checkbox" checked={check} onChange={e => setcheck(e.currentTarget.checked)} />
-      <input type="text" key='ok'/>
+      {
+        check ? (
+          <div>
+            <input type="text" key='ok'/>
+          </div>
+        ) :
+        (
+          <p>
+            <input type="text" key='ok'/>
+          </p>
+        )
+      }
     </>
   )
 }
