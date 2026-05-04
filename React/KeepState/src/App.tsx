34cf5cc44ecc715330b99943c6ea66a04faefb42
trikhaidev <trikhai.dev@gmail.com>
2026-05-04 20:37:28 +0700

    .

--- React/KeepState/src/App.tsx
+++ React/KeepState/src/App.tsx
@@ -5,13 +5,28 @@ import HelloWorld from "./components/HelloWorld";
 
 function App() {
   const [check, setcheck] = useState(true);
+  if (check) {
+    return (
+      <>
+        <HelloWorld show={!check}></HelloWorld>
+        {check ? <p>check true</p> : ' asas  as as asasd'}
+        <hr />
+        <Count check={check}></Count>
+        <Count check={check}></Count>
+        <input type="checkbox" checked={check} onChange={e => setcheck(e.currentTarget.checked)} />
+        <hr />
+        <input type="text" key='ok'/>
+      </>
+    );
+  }
   return (
     <>
       <HelloWorld show={!check}></HelloWorld>
       {check ? <p>check true</p> : ' asas  as as asasd'}
-      <Count></Count>
-      <Count></Count>
-      <input type="checkbox" checked = {check} onChange={e => setcheck(e.currentTarget.checked)}/>
+      <Count check={check}></Count>
+      <Count check={check}></Count>
+      <input type="checkbox" checked={check} onChange={e => setcheck(e.currentTarget.checked)} />
+      <input type="text" key='ok'/>
     </>
   )
 }
--- React/KeepState/src/components/Count.tsx
+++ React/KeepState/src/components/Count.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
-
-export default function Count(){
+type CountProps = {
+check:boolean
+};
+export default function Count(p:CountProps){
     const [currCount,setCurrCount] = useState(0);
+    if(p.check){
+        return (
+            <>
+                <h1>{currCount}</h1>
+                <h2>Check status: {p.check}</h2>
+                <button onClick={() => setCurrCount(currCount + 1)}>Next</button>
+            </>
+        );
+    }
     return (
         <>
             <h1>{currCount}</h1>
