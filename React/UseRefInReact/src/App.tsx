0c689283f3f04a098621d0430df6ca743195929e
trikhaidev <trikhai.dev@gmail.com>
2026-05-11 22:35:54 +0700

    Referencing Values with Refs
    Link: https://react.dev/learn/referencing-values-with-refs

--- React/UseRefInReact/src/App.tsx
+++ React/UseRefInReact/src/App.tsx
@@ -1,7 +1,24 @@
+import { useRef, useState } from "react"
 
 function App() {
+  const [text, setText] = useState('');
+  const [title,setTitle] = useState('');
+  const delay = useRef<null|number>(null);
   return (
     <>
+      <h1>The title is {title}</h1>
+      <input type="text" value={text} onChange={e => {
+        if(delay.current){
+          clearTimeout(delay.current);
+        }
+
+        const nextText = e.currentTarget.value;
+
+        setText(nextText);
+        delay.current = setTimeout(() => {
+          setTitle(nextText);
+        }, 1000);
+      }}/>
     </>
   )
 }
