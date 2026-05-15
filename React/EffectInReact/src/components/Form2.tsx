82184406a40d64d24abfbb122a61fb4f38229482
trikhaidev <trikhai.dev@gmail.com>
2026-05-15 20:04:36 +0700

    .

--- React/EffectInReact/src/components/Form2.tsx
+++ React/EffectInReact/src/components/Form2.tsx
@@ -1,17 +1,27 @@
+import { useRef } from "react";
+
 type Form2Props = {
     border:boolean
 }
 export default function Form2({border}:Form2Props){
+    const ref = useRef<HTMLInputElement|null>(null);
+
+    function handleClick(){
+        ref.current?.focus();
+    }
+
     if(border){
         return (
             <div style={{border:'2px solid black',padding:10}}>
-                <input type="text" />
+                <button onClick={handleClick}>Focus</button>
+                <input type="text" ref = {ref}/>
             </div>
         );
     }
     return(
         <div>
-            <input type="text" />
+            <button onClick={handleClick}>Focus</button>
+            <input type="text" ref = {ref}/>
         </div>
     );
 }
