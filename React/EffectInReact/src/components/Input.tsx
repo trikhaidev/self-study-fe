c37776b9836221965a0f0c7e8666dfeb02fbfd26
trikhaidev <trikhai.dev@gmail.com>
2026-05-18 22:31:15 +0700

    Không được phép setState khi render component

--- React/EffectInReact/src/components/Input.tsx
+++ React/EffectInReact/src/components/Input.tsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 
 export default function Input(){
+    const [count, setCount] = useState(0);
     const [text,setText] = useState('');
+    console.log("Input render");
+    // setCount(count + 1); không thể setState khi render component
+
+    // function myMethod(){
+    //     setCount(count + 1);
+    // }
+
+    // myMethod();
     return (
         <input type="text" value={text} onChange={e => setText(e.currentTarget.value)}/>
     );
