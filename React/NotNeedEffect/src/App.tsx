d659dd4205180f1347e55065d5aeeeb54f1bec0c
trikhaidev <trikhai.dev@gmail.com>
2026-05-19 20:54:51 +0700

    You Might Not Need an Effect
    Cập nhật trạng thái dựa trên props hoặc state: TypeFullName
    
    Link: https://react.dev/learn/you-might-not-need-an-effect

---
+++ React/NotNeedEffect/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import TypeFullName from './components/TypeFullname'
+
+function App() {
+  return (
+    <>
+      <TypeFullName></TypeFullName>
+    </>
+  )
+}
+
+export default App
---
+++ React/NotNeedEffect/src/components/TypeFullName.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+
+export default function TypeFullName() {
+    return (
+        <div style={
+            {
+                border:'2px solid black',
+                padding: 10
+            }
+        }>
+            <h1>Cập nhật trạng thái dựa trên props hoặc state</h1>
+            <div>
+                <h2>Cách 1: sử dụng Effect:</h2>
+                <WithEffect></WithEffect>
+            </div>
+            <div>
+                <h2>Cách 2: tối ưu hơn</h2>
+                <WithoutEffect></WithoutEffect>
+            </div>
+        </div>
+    );
+}
+
+function WithEffect() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [fullName, setFullName] = useState('');
+    useEffect(() => {
+        setFullName(`${firstName} ${lastName}`);
+        /**
+         * Cách này không tốt vì nó render với giá trị fullName cũ, ngay sau đó effect chạy và set lại fullName với value mới
+         * => Kích hoạt quá trình render lại. => Render 2 lần chỉ để cập nhật fullName ?
+         */
+    }, [firstName, lastName]);
+    return (
+        <>
+            <div>
+                <p>
+                    <label>
+                        FirstName: <input type="text" value={firstName} onChange={e => setFirstName(e.currentTarget.value)}></input>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        LastName: <input type="text" value={lastName} onChange={e => setLastName(e.currentTarget.value)}></input>
+                    </label>
+                </p>
+                <h3>FullName: {fullName}</h3>
+            </div>
+        </>
+    );
+}
+
+function WithoutEffect() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const fullName = `${firstName} ${lastName}`;
+    return (
+        <>
+            <div>
+                <p>
+                    <label>
+                        FirstName: <input type="text" value={firstName} onChange={e => setFirstName(e.currentTarget.value)}></input>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        LastName: <input type="text" value={lastName} onChange={e => setLastName(e.currentTarget.value)}></input>
+                    </label>
+                </p>
+                <h3>FullName: {fullName}</h3>
+            </div>
+        </>
+    );
+}
