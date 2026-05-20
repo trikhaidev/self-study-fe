f04dc0df55647021d7e4d2dfa4f217ee69967941
trikhaidev <trikhai.dev@gmail.com>
2026-05-20 23:06:58 +0700

    <SelectItem>Điều chỉnh một số trạng thái khi một thuộc tính thay đổi.</SelectItem>
    Link: https://react.dev/learn/you-might-not-need-an-effect

--- React/NotNeedEffect/src/App.tsx
+++ React/NotNeedEffect/src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Numbers from './components/Numbers'
 import ProfilePage from './components/ProfilePage'
+import SelectItem from './components/SelectItem'
 import TypeFullName from './components/TypeFullname'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <TypeFullName>Cập nhật trạng thái dựa trên props hoặc state</TypeFullName>
       <Numbers>Lưu trữ các phép tính tốn kém bằng cách sử dụng useMemo</Numbers>
       <ProfilePage>Khôi phục toàn bộ trạng thái khi một thuộc tính thay đổi.</ProfilePage>
+      <SelectItem>Điều chỉnh một số trạng thái khi một thuộc tính thay đổi.</SelectItem>
     </>
   )
 }
---
+++ React/NotNeedEffect/src/components/SelectItem.tsx
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from "react";
+
+type SelectItemProps = {
+    children:React.ReactNode
+}
+export default function SelectItem({children}:SelectItemProps){
+    const [text,setText] = useState('');
+    const [items, setItems] = useState<string[]>([]);
+    return (
+        <>
+            <h1>{children}</h1>
+            <input type="text" value={text} onChange={e => setText(e.currentTarget.value)}/>
+            <button onClick={() => setItems([...items,text])}>Add</button>
+            <ShowItem items={items} key = {items.length}></ShowItem>
+        </>
+    );
+}
+
+type ShowItemProps = {
+    items:string[]
+}
+function ShowItem({items}:ShowItemProps){
+    const [select,setSelect] = useState(0);
+    // const prevItems = useRef(items);
+    // if(prevItems.current !== items){
+    //     prevItems.current = items;
+    //     setSelect(0);
+    // } không thể truy cập ref trong quá trình reder ?
+
+    // const [prevItems, setPrevItems] = useState(items);
+    // if(prevItems !== items){
+    //     setSelect(0);
+    //     setPrevItems(items);
+    // } Cách này cũng không phải là tối ưu nhất. Cách tôi ưu nhất là truyền Key khi gọi ShowItem
+
+    return (
+        <>
+            <ul>
+                {
+                    items.map((x,i) => <li key={i}><button onClick={() => setSelect(i)}>{x}</button></li>)
+                }
+            </ul>
+            <h2>Chose: {items[select]}</h2>
+        </>
+    );
+}
+
