3800f9ad9cdff22c2283cfd8ded92202f7bda742
trikhaidev <trikhai.dev@gmail.com>
2026-05-26 23:19:51 +0700

    * Lưu ý: function sử dụng useEffectEvent này vẫn sẽ được định nghĩa lại sau mỗi lần render => nếu chày cối đưa nó vào mảng phụ thuộc thì Effect sẽ      * chạy lại sau mỗi lầnrender (do nó thấy function lần này khác với function của lần render trước đó)      *      * Lưu ý: vì đã sử dụng useEffectEvent nên ta k cần phải khai báo nó trong mảng phụ thuộc nữa. Nếu vẫn chày cối khai báo nó trong mảng phụ thuộc      * thì Effect vẫn sẽ chạy lại sau mỗi lần render.  Nói cách khác thì việc sử dụng useEffectEvent sẽ giống như việc bạn thông báo cho các Effect      * đang dùng function này là "Bạn không muốn Effect chạy lại chỉ vì function này được định nghĩa lại sau mỗi lần render"

--- React/NotNeedEffect/src/components/TypeSomeThing.tsx
+++ React/NotNeedEffect/src/components/TypeSomeThing.tsx
@@ -10,6 +10,9 @@ export default function TypeSomeThing(){
     /**
      * Nên dùng cách này nếu function được sử dụng trong effect nhưng bạn lại không muốn khai báo nó trong mảng phụ thuộc.
      * 
+     * Lưu ý: function sử dụng useEffectEvent này vẫn sẽ được định nghĩa lại sau mỗi lần render => nếu chày cối đưa nó vào mảng phụ thuộc thì Effect sẽ 
+     * chạy lại sau mỗi lầnrender (do nó thấy function lần này khác với function của lần render trước đó)
+     * 
      * Lưu ý: vì đã sử dụng useEffectEvent nên ta k cần phải khai báo nó trong mảng phụ thuộc nữa. Nếu vẫn chày cối khai báo nó trong mảng phụ thuộc
      * thì Effect vẫn sẽ chạy lại sau mỗi lần render.  Nói cách khác thì việc sử dụng useEffectEvent sẽ giống như việc bạn thông báo cho các Effect
      * đang dùng function này là "Bạn không muốn Effect chạy lại chỉ vì function này được định nghĩa lại sau mỗi lần render"
